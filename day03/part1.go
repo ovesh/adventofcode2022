1e940de08318f7f2bd86e23200cc0564ac89d181
Avishai Weissberg <avishai@deepgenomics.com>
2022-12-03 17:42:55 -0800

    use a set instead

--- day03/part1.go
+++ day03/part1.go
@@ -1,29 +1,8 @@
 package day03
 
 import (
-	"sort"
 	"strings"
 )
-
-type sortRunes []rune
-
-func (s sortRunes) Less(i, j int) bool {
-	return s[i] < s[j]
-}
-
-func (s sortRunes) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s sortRunes) Len() int {
-	return len(s)
-}
-
-func sortString(s string) string {
-	r := []rune(s)
-	sort.Sort(sortRunes(r))
-	return string(r)
-}
 
 func priority(r rune) int {
 	if r >= 'a' && r <= 'z' {
@@ -39,25 +18,17 @@ func Part1() int {
 	lines := strings.Split(input0, "\n")
 	total := 0
 	for _, line := range lines {
-		half1 := sortString(line[:len(line)/2])
-		half2 := sortString(line[len(line)/2:])
-		idx1 := 0
-		idx2 := 0
-		last := uint8(0)
-		for idx1 < len(half1) && idx2 < len(half2) {
-			if half1[idx1] == half2[idx2] {
-				if half1[idx1] != last {
-					last = half1[idx1]
-					total += priority(rune(last))
-				}
-				idx1++
-				idx2++
-			} else if half1[idx1] < half2[idx2] {
-				idx1++
-			} else {
-				idx2++
-			}
+		half1 := line[:len(line)/2]
+		half2 := line[len(line)/2:]
+		intersection := intersect(half1, half2)
+		if len(intersection) != 1 {
+			panic("unexpected intersection")
 		}
+		var key rune
+		for k := range intersection {
+			key = k
+		}
+		total += priority(key)
 	}
 	return total
 }
--- day03/part2.go
+++ day03/part2.go
@@ -4,48 +4,61 @@ import (
 	"strings"
 )
 
-func min(r1, r2, r3 uint8) uint8 {
-	if r1 < r2 && r1 < r3 {
-		return r1
+type runeset map[rune]struct{}
+
+func (r runeset) addAll(s string) {
+	for _, c := range s {
+		r[c] = struct{}{}
 	}
-	if r2 < r3 {
-		return r2
+}
+
+func (r runeset) contains(c rune) bool {
+	_, ok := r[c]
+	return ok
+}
+
+func intersect(s ...string) runeset {
+	result := runeset{}
+	sets := make([]runeset, len(s))
+	for i, s := range s {
+		sets[i] = runeset{}
+		sets[i].addAll(s)
 	}
-	return r3
+
+	for r := range sets[0] {
+		all := true
+		for i := 1; i < len(sets); i++ {
+			if !sets[i].contains(r) {
+				all = false
+				break
+			}
+		}
+		if all {
+			result[r] = struct{}{}
+		}
+	}
+	return result
 }
 
 func groupScore(elf1, elf2, elf3 string) int {
-	idx1 := 0
-	idx2 := 0
-	idx3 := 0
-	for idx1 < len(elf1) && idx2 < len(elf2) && idx3 < len(elf3) {
-		cur1 := elf1[idx1]
-		cur2 := elf2[idx2]
-		cur3 := elf3[idx3]
-		if cur1 == cur2 && cur2 == cur3 {
-			return priority(rune(cur1))
-		}
-		minRune := min(cur1, cur2, cur3)
-		if minRune == cur1 {
-			idx1++
-		}
-		if minRune == cur2 {
-			idx2++
-		}
-		if minRune == cur3 {
-			idx3++
-		}
+	intersection := intersect(elf1, elf2, elf3)
+	if len(intersection) != 1 {
+		panic("unexpected intersection")
 	}
-	panic("no match")
+	var key rune
+	for k := range intersection {
+		key = k
+	}
+	return priority(key)
 }
 
 func Part2() int {
 	lines := strings.Split(input0, "\n")
 	total := 0
 	for i := 0; i < len(lines); i += 3 {
-		elf1 := sortString(lines[i])
-		elf2 := sortString(lines[i+1])
-		elf3 := sortString(lines[i+2])
+		elf1 := lines[i]
+		elf2 := lines[i+1]
+		elf3 := lines[i+2]
 		total += groupScore(elf1, elf2, elf3)
 	}
 	return total
